4cb58f86a10b80e67ae7f2bf719ddb1b99bd4e38
Boomerster <28659272+doomestee@users.noreply.github.com>
2023-08-27 16:13:43 +0100

    Update the code, removes caching as will be hosted on serverless env

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const { existsSync, createReadStream, createWriteStream, writeFileSync } = require("fs");
-const { resolve } = require("path");
-const { request } = require("undici");
-const { pipeline } = require("stream/promises");
-const Cheerio = require("cheerio");
-const Location = require("./cache/location.json");
+// Since this will be on a serverless app (cyclic), no cache/package.json etc will be served.
 
-const fastify = require("fastify").default({logger: true});
-const PORT = process.env.PORT || 3000, devMode = false;
+const { request } = require("undici");
+const Cheerio = require("cheerio");
+
+const fastify = require("fastify").default({ logger: false});
+const PORT = process.env.PORT || 3000, devMode = true;
 
 fastify.route({
     method: 'GET', 
@@ -17,77 +15,40 @@ fastify.route({
         }
     },
     handler: async (req, reply) => {
-        function quick404(error='') {
-            return reply.code(400).header('Content-Type', 'application/json').send({ error });
-        }
+        const quick404 = (error='') => { return reply.code(400).header('Content-Type', 'application/json').send({ error }); }
 
-        if (!req.query['path']) return quick404("Missing path querystring. ");
-
+        if (!req.query['path']) return quick404("Missing path querystring.");
         if (!['.png'].some(file => req.query['path'].toLowerCase().endsWith(file.toLowerCase()))) return quick404("Recognised file type only: PNG"); //!req.query['path'].endsWith(".png"))
-
         if (['http', '/', 'ws', '.', 'file:', 'file'].some(indicator => req.query['path'].toLowerCase().startsWith(indicator))) return quick404("The path must be the file name, like this for example: 'Nightwraith.png' (excluding the quotation mark)");
 
         /**
          * @type {string}
          */
-        const path = req.query['path']//(req.query['path'].startsWith("/")) ? req.query['path'] : '/' + req.query['path'];
+        const path = req.query['path'];
         const redirect = (req.query['redirect'] === 'true') ? true : false; // ik but oh well shush.
 
-        // Check if it is already stored in cache.
-        if (redirect && Location[path]) return reply.redirect(Location[path]);
+        let result = await fetchImageLinkFromSource(path);
 
-        if (existsSync(resolve(__dirname + '/cache/' + path))) return reply.header("Content-Disposition", 'attachment; filename="poggus.png"').send(createReadStream(resolve(__dirname + '/cache/' + path)));
+        if (result.error) return quick404(result.error);
 
-        // If we made it here, looks like we're gonna have to fetch it! ^.^
-
-        // Checks if we already have the image source, but as we're here, this means we dont have the file downloaded.
-
-        try {
-            if (Location[path]) {
-                let img = await downloadImageFromLink(Location[path], resolve(__dirname + '/cache/' + path));
-
-                //console.log(redirect, req.query.redirect);
-                if (redirect) return reply.redirect(Location[path]);
-                if (img.success) return reply.header("Content-Disposition", 'attachment; filename="poggus.png"').send(createReadStream(resolve(__dirname + '/cache/' + path)));
-
-                return quick404(img.error);
-            }
-
-            // We made it here? so it didn't exist in the cache... sad sussy.
-
-            let link = await fetchImageLinkFromSource(path);
-
-            if (link.link) {
-                Location[path] = link.link;
-                writeFileSync(resolve(__dirname + '/cache/location.json'), JSON.stringify(Location)); // yes this will be problematic if we get like 50 links and a bunch of requests all at once, but I hugely doubt this API will require futureproofing.
-
-                //console.log(link);
-                if (redirect) return reply.redirect(link.link);
-
-                let imgA = await downloadImageFromLink(link.link, resolve(__dirname + '/cache/' + path));
-
-                if (imgA.success) return reply.header("Content-Disposition", 'attachment; filename="poggus.png"').send(createReadStream(resolve(__dirname + '/cache/' + path)));
-
-                return quick404(imgA.error);
-            }
-
-            return quick404(link.error);
+        if (redirect) return reply.redirect(result.link);
 
 
-        } catch (err) {
-            return quick404("There has been a problem either downloading or fetching the image");
-        }
-        
+        let image = await fetchImageFromLink(result.link);
+
+        if (image.error) return quick404(image.error);
+
+        return reply.send(image.body);
     }
 });
 
 /**
- * @param {string} link (relative, when i say link I mean something like 'nightwraith.png' or 'titan.png', basically file names)
- * @returns {Promise<{link?: string, error?: "Page doesn't exist."|"Errored trying to fetch/parse the image source's page."}>}
+ * @param {string} path (relative, when i say path I mean something like 'nightwraith.png' or 'titan.png', basically a file name)
+ * @returns {Promise<{link: string}|{error: "Page doesn't exist."|"Errored trying to fetch/parse the image source's page."}>}
  */
-async function fetchImageLinkFromSource(link) {
+async function fetchImageLinkFromSource(path) {
     try {
-        const { body, statusCode } = await request("https://epicduelwiki.com/w/File:" + link, { maxRedirections: 1, method: "GET", headersTimeout: 1000 * 60 * 5, bodyTimeout: 0});
+        const { body, statusCode } = await request("https://epicduelwiki.com/w/File:" + path, {method: "GET", headersTimeout: 1000 * 60 * 5, bodyTimeout: 0});
 
         if (statusCode === 404) return {error: "Page doesn't exist."};
 
@@ -105,32 +66,22 @@ async function fetchImageLinkFromSource(link) {
 }
 
 /**
- * Downloads the image content
+ * Fetches the raw image
  * @param {string} link (full link)
- * @param {string} finalPath path to where the file downloaded should be at (and also its name)
- * @returns {Promise<{success: true, error: "Page doesn't exist."|"Errored trying to download the image."}>}
+ * @returns {Promise<{success: true, body: any}|{error: "Page doesn't exist."}>}
  */
-async function downloadImageFromLink(link, finalPath) {
-    try {
-        const { body, statusCode } = await request(link, { maxRedirections: 1, headersTimeout: 1000 * 60 * 5, bodyTimeout: 0});
+async function fetchImageFromLink(link) {
+    const { body, statusCode } = await request(link, { headersTimeout: 1000 * 60 * 5, bodyTimeout: 0});
 
-        if (statusCode === 404) return {error: "Page doesn't exist."};
+    if (statusCode === 404) return {error: "Page doesn't exist."};
 
-        //let piping = body.pipe(createWriteStream(finalPath));
-
-        await pipeline(body, createWriteStream(finalPath));
-
-        return { success: true }
-    } catch (err) {
-        console.log(err);
-        return {error: "Errored trying to download the image."};
-    }
+    return { success: true, body };
 }
 
 fastify.setErrorHandler((error, req, res) => {
     if (error) console.log(error); //idk
-})
+});
 
-fastify.listen(PORT, (devMode) ? '127.0.0.1' : '0.0.0.0').then(() => {
-    console.log("Listening to " + ((devMode) ? '127.0.0.1' : '0.0.0.0') + ":" + PORT);
-}, console.log);
+fastify.listen({
+    port: PORT, host: (devMode) ? '127.0.0.1' : '0.0.0.0'
+}).then(() => console.log("Listening to " + ((devMode) ? '127.0.0.1' : '0.0.0.0') + ":" + PORT), console.log);
